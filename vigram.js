aa8c43e5007233e8d97e8e185b5f0bf51f0844b9
Neodern <neodern@gmail.com>
2014-03-03 23:30:43 +0100

    docs.

--- vigram.js
+++ vigram.js
@@ -1,3 +1,10 @@
+/**
+ * Vigram
+ * @version : 1.0
+ * @author: Nicolas (@neodern) Jamet <neodern@gmail.com>
+ * @about:
+ */
+
 
 /**
  *
@@ -24,100 +31,85 @@ function getTypeFromInstagramMedia(content) {
     return false;
 }
 
-    var image = chrome.extension.getURL("medias/images/vigram_128.png");
 
-    var getRealImgFromInstagram = (function (content) {
-        var url = getUrlFromInstagramMedia(content);
-        var is_pic = getTypeFromInstagramMedia(content);
-        if (!is_pic) {
-            url = url.replace("s3.amazonaws", "ak.instagram");
-            url = url.replace("_6.", "_7.");
-        }
-        return url;
-    });
+/**
+ *
+ * @param content
+ * @returns {string}
+ */
+function getRealImgFromInstagram(content) {
+    var url = getUrlFromInstagramMedia(content);
+    var is_pic = getTypeFromInstagramMedia(content);
+    if (!is_pic) {
+        url = url.replace("s3.amazonaws", "ak.instagram");
+        url = url.replace("_6.", "_7.");
+    }
+    return url;
+}
+
+var image = chrome.extension.getURL("medias/images/vigram_128.png");
 
 
-    /**
-     * Event when mouse enter or leave a pic/video block
-     */
-    $('.photo-feed').on('mouseenter', '.photo', function () {
-        $(this).find('.VigramEffect').css('width', '25px');
-    });
-    $('.photo-feed').on('mouseleave', '.photo', function () {
-        $(this).find('.VigramEffect').css('width', '0');
-    });
+/**
+ * Event when mouse enter or leave a pic/video block
+ */
+$('.photo-feed').on('mouseenter', '.photo', function () {
+    $(this).find('.VigramEffect').css('width', '25px');
+});
+$('.photo-feed').on('mouseleave', '.photo', function () {
+    $(this).find('.VigramEffect').css('width', '0');
+});
 
-    /**
-     * Event Profile Instagram Loading
-     */
-    $('.photo-feed').ready(function () {
-        $('.photo-wrapper').each(function () {
-            var $this = $(this);
-            if (($this.hasClass('Vigram')))
-                return;
 
-            var urlToMedia = $this.find('a').first().attr('href');
-            $.get(urlToMedia, function (content) {
-                var url = getRealImgFromInstagram(content);
-                var fName = url.split("/")[3];
-                $this.addClass('Vigram');
-                $('<a>', {class: "VigramProfileButton", href: url, download: fName})
-                    .append($("<img>", {class: "VigramEffect size25", src: image}))
-                    .appendTo($this);
-            });
-        });
-    });
-
-    /**
-     * Event Profile Instagram NewNode
-     */
-    $('.photo-feed').on('DOMNodeInserted', '.photo', function (e) {
-        var $elem = $(e.target);
-        var $children = $elem.children().first();
-        if ($elem.hasClass('Vigram') || $elem.hasClass('VigramProfileButton'))
+/**
+ * Event Profile Instagram Loading
+ */
+$('.photo-feed').ready(function () {
+    $('.photo-wrapper').each(function () {
+        var $this = $(this);
+        if (($this.hasClass('Vigram')))
             return;
 
-        var urlToMedia = $elem.find('a').first().attr('href');
+        var urlToMedia = $this.find('a').first().attr('href');
         $.get(urlToMedia, function (content) {
             var url = getRealImgFromInstagram(content);
             var fName = url.split("/")[3];
-            $elem.addClass('Vigram');
-            $("<a>", {class: "VigramProfileButton", href: url, download: fName})
-                .append($("<img>", {class: "VigramEffect size25", src: image}))
-                .appendTo($children);
-        });
-    });
-
-    /**
-     * Event Timeline Instagram Loading.
-     */
-    $('.timelineContainer').ready(function () {
-        $('.timelineItem').each(function () {
-            var $this = $(this);
-            if ($this.hasClass('Vigram'))
-                return;
-
-            var url = $this.find('.Video').first().attr('src');
-            if (typeof url == 'undefined')
-                url = $this.find('.timelinePhoto').attr('src');
             $this.addClass('Vigram');
-            if (typeof url != 'undefined') {
-                var fName = url.split("/")[3];
-                $this.find('.timelineLikeButton').after(
-                    $("<a>", {class: "timelineLikeButton", style: "background:none;", href: url, download: fName})
-                        .append($("<div>", {class: "Vcenter"})
-                            .append($("<img>", {class: "size25", src: image}))
-                        )
-                    );
-            }
+            $('<a>', {class: "VigramProfileButton", href: url, download: fName})
+                .append($("<img>", {class: "VigramEffect size25", src: image}))
+                .appendTo($this);
         });
     });
+});
 
-    /**
-     * Event Timeline Instagram NewNode
-     */
-    $('.timelineContainer').on('DOMNodeInserted', '.timelineItem', function (e) {
-        var $this = $(e.target);
+
+/**
+ * Event Profile Instagram NewNode
+ */
+$('.photo-feed').on('DOMNodeInserted', '.photo', function (e) {
+    var $elem = $(e.target);
+    var $children = $elem.children().first();
+    if ($elem.hasClass('Vigram') || $elem.hasClass('VigramProfileButton'))
+        return;
+
+    var urlToMedia = $elem.find('a').first().attr('href');
+    $.get(urlToMedia, function (content) {
+        var url = getRealImgFromInstagram(content);
+        var fName = url.split("/")[3];
+        $elem.addClass('Vigram');
+        $("<a>", {class: "VigramProfileButton", href: url, download: fName})
+            .append($("<img>", {class: "VigramEffect size25", src: image}))
+            .appendTo($children);
+    });
+});
+
+
+/**
+ * Event Timeline Instagram Loading.
+ */
+$('.timelineContainer').ready(function () {
+    $('.timelineItem').each(function () {
+        var $this = $(this);
         if ($this.hasClass('Vigram'))
             return;
 
@@ -135,59 +127,86 @@ function getTypeFromInstagramMedia(content) {
                 );
         }
     });
+});
 
-    /**
-     * Event pic/video page Instagram.
-     */
-    $('.lbAnimation').ready(function () {
-        $.get(null, function (content) {
-            var url = getUrlFromInstagramMedia(content);
-            if (typeof url === 'undefined')
-                return;
-            var fName = url.split("/")[3];
-            if (typeof fName === 'undefined' || fName === 'profiles')
-                return;
-            var is_pic = getTypeFromInstagramMedia(content);
-            if (is_pic)
-                var text_button = chrome.i18n.getMessage("dl_button_pic");
-            else
-                var text_button = chrome.i18n.getMessage("dl_button_vid");
-            $('.top-bar-actions').ready(function () {
-                if (!($('#VigramSingleImg').length)) {
-                    var topBar = $('.top-bar-home').attr('id', '');
-                    topBar.children().attr('id', '');
 
-                    $("<li>", {id: "VigramSingleImg"})
-                        .append($("<a>", {href: url, download: fName})
-                            .append($("<span>", {class: "img-outset"})
-                                .append($("<img>", {src: image}))
-                            )
-                            .append($("<strong>").text(text_button))
-                        )
-                        .appendTo($('.top-bar-actions').first());
+/**
+ * Event Timeline Instagram NewNode
+ */
+$('.timelineContainer').on('DOMNodeInserted', '.timelineItem', function (e) {
+    var $this = $(e.target);
+    if ($this.hasClass('Vigram'))
+        return;
 
-                    $('#VigramSingleImg').animate({ marginTop: '0px' }, 1500);
-                }
-            });
-        });
-    });
+    var url = $this.find('.Video').first().attr('src');
+    if (typeof url == 'undefined')
+        url = $this.find('.timelinePhoto').attr('src');
+    $this.addClass('Vigram');
+    if (typeof url != 'undefined') {
+        var fName = url.split("/")[3];
+        $this.find('.timelineLikeButton').after(
+            $("<a>", {class: "timelineLikeButton", style: "background:none;", href: url, download: fName})
+                .append($("<div>", {class: "Vcenter"})
+                    .append($("<img>", {class: "size25", src: image}))
+                )
+            );
+    }
+});
 
-    /**
-     * Event Vine. \o/
-     */
-    $('.info-row').ready(function () {
-        var url = $('.vine-video-container').children().attr('src');
+
+/**
+ * Event pic/video page Instagram.
+ */
+$('.lbAnimation').ready(function () {
+    $.get(null, function (content) {
+        var url = getUrlFromInstagramMedia(content);
         if (typeof url === 'undefined')
             return;
-        var splittedUrl = url.split('/')[5];
-        var name = splittedUrl.substring(0, splittedUrl.indexOf('.'));
+        var fName = url.split("/")[3];
+        if (typeof fName === 'undefined' || fName === 'profiles')
+            return;
+        var is_pic = getTypeFromInstagramMedia(content);
+        if (is_pic)
+            var text_button = chrome.i18n.getMessage("dl_button_pic");
+        else
+            var text_button = chrome.i18n.getMessage("dl_button_vid");
+        $('.top-bar-actions').ready(function () {
+            if (!($('#VigramSingleImg').length)) {
+                var topBar = $('.top-bar-home').attr('id', '');
+                topBar.children().attr('id', '');
 
-        if (!($('#VineButton').length)) {
-            var info = $('.info');
-            $("<div>", {class: "action-container"})
-                .append($("<a>", {id: "VineButton", href: url, download: name})
-                    .append($("<img>", {src: image, width:"35px"})))
-                .append($("<font>", {style: "font-size:11px;"}).text("Vigram"))
-                .appendTo($('.post-engagement'));
-        }
+                $("<li>", {id: "VigramSingleImg"})
+                    .append($("<a>", {href: url, download: fName})
+                        .append($("<span>", {class: "img-outset"})
+                            .append($("<img>", {src: image}))
+                        )
+                        .append($("<strong>").text(text_button))
+                    )
+                    .appendTo($('.top-bar-actions').first());
+
+                $('#VigramSingleImg').animate({ marginTop: '0px' }, 1500);
+            }
+        });
     });
+});
+
+
+/**
+ * Event Vine. \o/
+ */
+$('.info-row').ready(function () {
+    var url = $('.vine-video-container').children().attr('src');
+    if (typeof url === 'undefined')
+        return;
+    var splittedUrl = url.split('/')[5];
+    var name = splittedUrl.substring(0, splittedUrl.indexOf('.'));
+
+    if (!($('#VineButton').length)) {
+        var info = $('.info');
+        $("<div>", {class: "action-container"})
+            .append($("<a>", {id: "VineButton", href: url, download: name})
+                .append($("<img>", {src: image, width:"35px"})))
+            .append($("<font>", {style: "font-size:11px;"}).text("Vigram"))
+            .appendTo($('.post-engagement'));
+    }
+});
